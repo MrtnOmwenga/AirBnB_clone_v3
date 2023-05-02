798fdd07027faeefcc8ba5679be3921e52187e6d
MrtnOmwenga <martin36449@gmail.com>
2023-05-01 19:40:53 -0700

    Pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,6 +12,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
